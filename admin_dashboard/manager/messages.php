15e6babacc22424835c555ae2d1011bb9e9d1db9
Jackson SANKARA <135215771+jacksonsankara@users.noreply.github.com>
2023-11-21 18:55:51 +0200

    Add files via upload

---
+++ admin_dashboard/manager/messages.php
@@ -0,0 +1,220 @@
+<?php
+// Database connection
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "almanncoffeehouse";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Count number of users
+$query = "SELECT COUNT(*) as count FROM messages";
+$result = mysqli_query($conn, $query);
+$row = mysqli_fetch_assoc($result);
+$count = $row['count'];
+
+// Retrieve all users
+$query = "SELECT * FROM messages";
+$result = mysqli_query($conn, $query);
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+      <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Simple Responsive Admin</title>
+    <!-- BOOTSTRAP STYLES-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+     <!-- FONTAWESOME STYLES-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+        <!-- CUSTOM STYLES-->
+    <link href="assets/css/custom.css" rel="stylesheet" />
+     <!-- GOOGLE FONTS-->
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+   <style>
+        .container {
+            margin-top: 50px;
+        }
+
+        h1 {
+            text-align: center;
+            margin-bottom: 30px;
+        }
+
+        .table td,
+        .table th {
+            vertical-align: middle;
+        }
+
+        .action-btns {
+            display: flex;
+            justify-content: center;
+        }
+
+        .action-btns button {
+            margin: 0 10px;
+        }
+        /* Custom CSS */
+table {
+    border-collapse: collapse;
+    width: 100%;
+}
+
+th, td {
+    text-align: left;
+    padding: 8px;
+}
+
+th {
+    background-color: #333;
+    color: #fff;
+}
+
+tr:nth-child(even) {
+    background-color: #f2f2f2;
+}
+
+button {
+    background-color: skyblue;
+    color: white;
+    padding: 6px 10px;
+    border: none;
+    border-radius: 3px;
+    cursor: pointer;
+    margin-right: 5px;
+}
+
+button:hover {
+    background-color: #3e8e41;
+}
+
+
+  </style>
+</head>
+<body>
+     
+           
+          
+    <div id="wrapper">
+         <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="adjust-nav">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">
+                        <img src="assets/img/alman_logo.png" style="margin-top: -10px;" />
+
+                    </a>
+                    
+                </div>
+              
+                <span class="logout-spn" >
+                  <a href="logout.php"><button class="btn btn-primary" style="margin-top: 12px;">Logout</button></a>
+                   
+
+                </span>
+            </div>
+        </div>
+        <!-- /. NAV TOP  -->
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+                 
+
+ <li >
+                        <a href="index.html" ><i class="fa fa-desktop "></i>Dashboard <span class="badge">Included</span></a>
+                    </li>
+                   
+
+                    <li>
+                        <a href="ui.html"><i class="fa fa-table "></i>UI Elements  <span class="badge">Included</span></a>
+                    </li>
+                    <li class="active-link">
+                        <a href="blank.html"><i class="fa fa-edit "></i>Blank Page  <span class="badge">Included</span></a>
+                    </li>
+
+
+
+                </ul>
+                            </div>
+
+        </nav>
+        <!-- /. NAV SIDE  -->
+        <div id="page-wrapper" >
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+ 
+</head>
+<body style="background-color:LightSalmon ;">
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                
+            </div>
+        </nav>
+    </header>
+    <div class="container">
+        <h1>Messages Received</h1>
+        <div class="row">
+            <div class="col-md-2">
+                <div class="card text-center" style="border-radius: 12px;box-shadow: 2px 4px 2px 4px skyblue;height: 250px;">
+                    <div class="card-header">
+                        Total Messages
+                    </div>
+                    <div class="card-body">
+                        <h1 style="font-size: 100px;"><?php echo $count; ?></h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-9">
+                <div class="table-responsive">
+                    <?php 
+            include "check_messages.php"; 
+        ?>
+                </div>
+            </div>
+        </div>
+    </div></center>
+  </main>
+
+ 
+
+
+                    </div>
+                </div>              
+                 <!-- /. ROW  -->
+                  <hr />
+              
+                 <!-- /. ROW  -->           
+    </div>
+            
+            </div>
+         <!-- /. PAGE WRAPPER  -->
+
+        </div>
+    <div class="footer">
+      
+    
+             <div class="row">
+                <div class="col-lg-12" >
+                    &copy;  2014 yourdomain.com | Design by: <a href="http://binarytheme.com" style="color:#fff;"  target="_blank">www.binarytheme.com</a>
+                </div>
+        </div>
+        </div>
+          
+
+     <!-- /. WRAPPER  -->
+    <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+    <!-- JQUERY SCRIPTS -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+      <!-- BOOTSTRAP SCRIPTS -->
+    <script src="assets/js/bootstrap.min.js"></script>
+      <!-- CUSTOM SCRIPTS -->
+    <script src="assets/js/custom.js"></script>
+    
+   
+</body>
+</html>
