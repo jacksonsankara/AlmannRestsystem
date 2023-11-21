da2948b5eb3461a5537442115a67b94cd0934b52
Jackson SANKARA <135215771+jacksonsankara@users.noreply.github.com>
2023-11-21 19:03:38 +0200

    Update messages.php

--- admin_dashboard/messages.php
+++ admin_dashboard/messages.php
@@ -17,7 +17,7 @@ $query = "SELECT * FROM messages";
 $result = mysqli_query($conn, $query);
 ?>
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head>
       <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
